8d90298b483c195746681d188139eb1113d0ef39
JosePradoGaso <jpradogaso@gmail.com>
2025-12-22 12:17:04 +0100

    SDFSDF

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = requiere ("express");
+const app = express();
+
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+    console.log("hola desde el puerto:" + app.get("port"));
+});
