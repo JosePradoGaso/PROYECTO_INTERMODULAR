66eb1f0cffa038e0d6e386d05eb2b37deab6fe20
Yasmin Boada <yaboan@alumnatflorida.es>
2025-12-22 13:06:22 +0100

    Revert "local host"
    This reverts commit 2301dd96eaeed382791110d731f74ac8dfe67fc1.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-const express = require("express");
+const express = require ("express");
 const app = express();
 
 app.set("port", process.env.PORT || 8080);
 
 app.listen(app.get("port"), () => {
-  console.log("hola desde el puerto:" + app.get("port"));
+    console.log("hola desde el puerto:" + app.get("port"));
 });
 
 app.get("/", (req, res) => {
-  console.log("Alguien accedió al servidor");
-  res.send("Bienvenido al servidor");
+    console.log ("Alguien accedió al servidor");
+    res.send ("Bienvenido al servidor");
 });
 
 app.get("/rutaGet", (req, res) => {
-  console.log("Alguien hizo un get");
-  res.send("Hola desde la ruta get");
+    console.log ("Alguien hizo un get");
+    res.send ("Hola desde la ruta get");
 });
 
+
 app.get("/carrito", (req, res) => {
-  console.log("Alguien hizo un get");
-  res.send("Este es tu carrito de compras");
+    console.log ("Alguien hizo un get");
+    res.send ("Este es tu carrito de compras");
 });
