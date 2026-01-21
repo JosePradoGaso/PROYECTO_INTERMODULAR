1ddb4df6303a067049a0d33a1236aa821d307a45
JosePradoGaso <jpradogaso@gmail.com>
2026-01-21 12:35:14 +0100

    Instalacion del modulo Morgan

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const express = require ("express"); //express: será el encargado de configurar las funcionalidades básicas del servidor
-
 const app = express();
+const morgan = require("morgan");
 
+//Escribiendo en consola npm run dev activaremos al módulo.
+
+//Settings
 app.set("port", process.env.PORT || 8080); //El puerto 8080 lo fijamos como puerto de inicio, nos servirá para hacer las pruebas
 
+//Middleware
+app.use(morgan("dev"));
+
+//Rutas
+app.use(require("./routes/index")); //lamada del módulo recién creado en el archivo inicial, para que funcione la ruta del otro archivo
+
+// Iniciando el servidor
 app.listen(app.get("port"), () => {
     console.log("hola desde el puerto:" + app.get("port")); //Esto hace una peticion get al puerto que hemos marcado con ese mensaje
 });
-
-
-app.use(require("./routes/index")); //lamada del módulo recién creado en el archivo inicial, para que funcione la ruta del otro archivo
-
-
-
 
